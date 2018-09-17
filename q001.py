dd68bf1333ab853e356d51cd89f93efa7b301bfa
takeshi <kagome>
2018-09-17 15:12:28 +0900

    q001 test commit

---
+++ q001.py
@@ -0,0 +1,16 @@
+num = 11
+while True:
+    if str(num) == str(num)[::-1] and \
+            format(num, 'b') == format(num, 'b')[::-1] and \
+            format(num, 'o') == format(num, 'o')[::-1]:
+        print(num)
+        break
+
+    num += 1
+
+# 文字列の反転
+# 基数変換
+# and処理の書き方
+# 改行の仕方、改行コードの表示
+
+# コードの整形
